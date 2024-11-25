879717de00baeb90d1d474ea6e8838ab38fe5b89
enqack <enqack@gmail.com>
2024-11-25 14:31:16 -0500

    Yubikey setup for sysadm.

--- users/sysadm/home.nix
+++ users/sysadm/home.nix
@@ -27,6 +27,16 @@
     "vvcccbldbvtd"
   ];
 
+  programs.gpg = {
+    enable = true;
+    homedir = "${config.xdg.dataHome}/gnupg";
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+  };
+
   imports = [
     ../../profiles/base
 
