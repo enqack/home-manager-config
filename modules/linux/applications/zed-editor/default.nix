b033b4ad06b2336dd154aa9ebc2e463a72c29698
enqack <enqack@gmail.com>
2026-05-11 03:40:02 -0400

    Added settings in zed-editor module for linux.

--- modules/linux/applications/zed-editor/default.nix
+++ modules/linux/applications/zed-editor/default.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   options.modules.applications.zed-editor.enable = lib.mkEnableOption "zed-editor";
@@ -10,16 +15,39 @@
     programs.zed-editor = {
       enable = true;
       package = pkgs.unstable.zed-editor;
-      
-      extensions = [ "nix" "tmol" "yaml" "json" "jsonl" "go" "python" ];
+
+      extensions = [
+        "nix"
+        "tmol"
+        "yaml"
+        "json"
+        "jsonl"
+        "go"
+        "proto"
+        "python"
+      ];
+
       userSettings = {
+        auto_update = false;
         theme = {
           mode = "system";
           dark = "One Dark";
           light = "One Light";
         };
-      };
-    };
+        features = {
+          copilot = false;
+        };
 
+        lsp = {
+          nix = {
+            binary = {
+              path_lookup = true;
+            };
+          };
+        };
+
+      };
+
+    };
   };
 }
